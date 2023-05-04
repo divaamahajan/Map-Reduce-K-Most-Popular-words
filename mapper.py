d03605f48f9dab1ccac68e5bea9758b4c3dbe7a9
Divya Mahajan <102075299+divaamahajan@users.noreply.github.com>
2023-05-03 19:40:20 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -14,48 +14,23 @@ def read_stop_words(file_path):
         return stop_words
 
 
-def read_chunk(file_path, chunk_start, chunk_size):
-    with open(file_path, "r") as file:
-        file.seek(chunk_start)
-        chunk = file.read(chunk_size)
-    return chunk
-
-
-def process_chunk(chunk, stop_words):
-    word_counts = defaultdict(int)
-    for line in chunk.splitlines():
-        for word in re.findall(r"\w+", line.lower()):
-            if word and word not in stop_words:
-                word_counts[word] += 1
-    return word_counts
-
-
-def main():
+def mapper():
     # read stop words from file
     stop_words = read_stop_words("stop_words.txt")
-
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_file", help="Path to input file")
-    args = parser.parse_args()
-
-    # read the input data
-    input_file = args.input_file
-    file_size = os.path.getsize(input_file)
-    chunk_size = 64 * 1024 * 1024 # 64 MB
-
-    # create a list of chunks
-    chunks = []
-    offset = 0
-    while offset < file_size:
-        chunk_end = min(offset + chunk_size, file_size)
-        chunks.append((offset, chunk_end))
-        offset = chunk_end
-
-    # process each chunk and output the intermediate key-value pairs
-    for chunk_start, chunk_end in chunks:
-        chunk = read_chunk(input_file, chunk_start, chunk_size)
-        word_counts = process_chunk(chunk, stop_words)
+    # input comes from STDIN (standard input)
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # split the line into words
+        words = re.findall(r"\w+", line.lower())
+        # create a dictionary to store the word counts
+        word_counts = defaultdict(int)
+        # iterate over the words
+        for word in words:
+            # if the word is not in the stop words set, increment its count in the dictionary
+            if word not in stop_words:
+                word_counts[word] += 1
+        # emit the word counts as key-value pairs
         for word, count in word_counts.items():
             print(f"{word}\t{count}")
 
