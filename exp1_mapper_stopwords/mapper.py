be68b23c6e0f18c4e8ec235831211e306c56ed70
Rushang Shah <rushshah@Rushangs-MacBook-Pro.local>
2023-05-09 16:32:03 -0700

    Adding generateLogs() functions

--- exp1_mapper_stopwords/mapper.py
+++ exp1_mapper_stopwords/mapper.py
@@ -5,7 +5,7 @@ import sys
 import os
 import logging
 import time
-
+import psutil
 
 # get the absolute path of the directory 
 dir_path = os.path.dirname(os.path.dirname(__file__))
@@ -19,6 +19,12 @@ def read_stop_words(file_path):
 
 
 def mapper():
+    # Get the file descriptor of sys.stdin
+    fd = sys.stdin.fileno()
+
+    # Get the size of the file descriptor
+    size = os.fstat(fd).st_size
+
     # read stop words from file
     stop_words = read_stop_words(STOP_WORDS)
     # create a dictionary to store the word counts
@@ -36,10 +42,24 @@ def mapper():
     # emit the word counts as key-value pairs
     for word, count in word_counts.items():
         print(f"{word}\t{count}")
+    
+    return size
+
+def generateLogs(fileSize, start_time, end_time):
+    running_time = end_time - start_time
+    PID = os.getpid()
+    process = psutil.Process(PID)
+    memory_usage = process.memory_info().rss
+    cpu_utilization = psutil.cpu_percent(interval=running_time)
+    logging.info(f"{PID}, {fileSize /1024 /1024: .2f}, {running_time:.2f}, {memory_usage / 1024 / 1024:.2f}, {cpu_utilization:.2f}")
+    # logging.info(f"********************* Mapper Memory usage: {memory_usage / 1024 / 1024:.2f} MB")
+    # logging.info(f"********************* Mapper CPU utilization: {cpu_utilization:.2f} %")
+
 
 if __name__ == '__main__':    
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.basicConfig(filename='mapper.csv',format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.info('PID, File read size(MB), Runtime(seconds), Memory Usage(MB), CPU utilization (%)')
     start_time = time.time()
-    mapper()
+    fileSize = mapper()
     end_time = time.time()
-    logging.info(f"********************* Mapper Total runtime: {end_time - start_time} seconds")
+    generateLogs(fileSize,start_time,end_time)
--- exp1_mapper_stopwords/reducer.py
+++ exp1_mapper_stopwords/reducer.py
@@ -3,8 +3,15 @@ import sys
 from collections import defaultdict
 import logging
 import time
+import os
+import psutil
 
 def reducer():
+    # Get the file descriptor of sys.stdin
+    fd = sys.stdin.fileno()
+
+    # Get the size of the file descriptor
+    size = os.fstat(fd).st_size
     # create a dictionary to store the word counts
     word_counts = defaultdict(int)
     
@@ -28,10 +35,23 @@ def reducer():
     # emit the word counts as key-value pairs
     for word, count in word_counts.items():
         print(f"{word}\t{count}")
+    
+    return size
+
+def generateLogs(fileSize, start_time, end_time):
+    running_time = end_time - start_time
+    PID = os.getpid()
+    process = psutil.Process(PID)
+    memory_usage = process.memory_info().rss
+    cpu_utilization = psutil.cpu_percent(interval=running_time)
+    logging.info(f"{PID}, {fileSize /1024 /1024: .2f}, {running_time:.2f}, {memory_usage / 1024 / 1024:.2f}, {cpu_utilization:.2f}")
+    # logging.info(f"********************* Reducer Memory usage: {memory_usage / 1024 / 1024:.2f} MB")
+    # logging.info(f"********************* Reducer CPU utilization: {cpu_utilization:.2f} %")
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.basicConfig(filename='reducer.csv',format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.info('PID, File read size (MB), Runtime(seconds), Memory Usage(MB), CPU utilization (%)')
     start_time = time.time()
-    reducer()
+    fileSize = reducer()
     end_time = time.time()
-    logging.info(f"********************* Reducer Total runtime: {end_time - start_time} seconds")
+    generateLogs(fileSize, start_time,end_time)
